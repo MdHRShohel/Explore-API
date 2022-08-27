8b6798ab938070e955315d707e328e771a4c9cc3
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-08-27 23:46:13 +0600

    API

---
+++ api.js
@@ -0,0 +1,2 @@
+/*What is an API? (Application Programming Interface)
+API is the acronym for Application Programming Interface, which is a software intermediary that allows two applications to talk to each other. Each time you use an app like Facebook, send an instant message, or check the weather on your phone, you’re using an API. */
---
+++ json.js
@@ -0,0 +1,3 @@
+const user = {id:1, name: 'Amir', job: actor};
+
+console.log(user);
