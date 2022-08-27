2bf5fef907a58dfdd5831c2e265841d2910c4b75
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-08-28 00:50:06 +0600

    Dynamically display loaded data on your website

--- js/app.js
+++ js/app.js
@@ -10,6 +10,15 @@ function loadUsers(){
 }
 
 
+// function displayUsers(data){
+//     console.log(data);
+// }
 function displayUsers(data){
-    console.log(data);
+    const ul = document.getElementById('users-list');
+    for(const user of data){
+        // console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
+    }
 }
