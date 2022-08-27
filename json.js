2b7f98bc7e28003a0b63979a8e9cbc3662c2948a
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-08-27 23:54:05 +0600

    simple stringify example added

--- json.js
+++ json.js
@@ -1,3 +1,9 @@
-const user = {id:1, name: 'Amir', job: actor};
-
+const user = {id: 1, name: 'Amir', job: 'actor'};
+//JavaScript Object Notation(JSON) 
+const stringified = JSON.stringify(user);
 console.log(user);
+console.log(stringified);
+/*OUTPUT:
+{ id: 1, name: 'Amir', job: 'actor' }
+{"id":1,"name":"Amir","job":"actor" 
+*/
