531a30b864f18f18ceacf3b4627efc3f3d90e1ac
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-08-28 00:02:21 +0600

    parse example added

--- json.js
+++ json.js
@@ -7,3 +7,24 @@ console.log(stringified);
 { id: 1, name: 'Amir', job: 'actor' }
 {"id":1,"name":"Amir","job":"actor" 
 */
+
+const shop = {
+    owner: 'Alia',
+    address: {
+        street : 'shiyalbari more road',
+        city: 'Dhaka',
+        country : 'Bangladesh'
+    },
+    product : ['laptop','microphone','keyboard'],
+    revenue : 45000,
+    isOpen : true,
+    isNew : false
+};
+
+console.log(shop);
+//stringify convert object to string
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
+//parse convert string to object
+const shopObj = JSON.parse(shopJSON);
+console.log(shopObj);
